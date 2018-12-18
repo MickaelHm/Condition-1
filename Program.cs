0c4128bb335a9a2df27cdeeb07a928d420678516
MickaelHm <harmeau.mickael@gmail.com>
2018-12-18 16:01:52 +0100

    Complet

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condition_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool rainy = false;
+            if (rainy)
+            {
+                Console.WriteLine("Bonjour, il pleut aujourd'hui, vous devriez prendre votre parapluie.");
+            }
+            else
+            {
+                Console.WriteLine("Bonjour, il ne pleut pas aujourd'hui, bonne journée.");
+            }
+        }
+    }
+}
